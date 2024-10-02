4de97038c866ca87081d6c26c1c358ce931609ae
alibaba0010 <zakariyahali100@gmail.com>
2024-10-02 07:02:36 +0100

    weq;iowhifvdgio;gr

--- packages/backend/routes/authorisationRoutes.js
+++ packages/backend/routes/authorisationRoutes.js
@@ -9,18 +9,18 @@ import allPosts from "../controllers/access/controls/allPosts.js";
 const accessControlRoutes = Router({ mergeParams: true });
 
 accessControlRoutes.post("/post/create", tokenVerification, (req, res) =>
-	createPost(req, res),
+  createPost(req, res)
 );
 accessControlRoutes.delete("/post/delete", tokenVerification, (req, res) =>
-	deletePost(req, res),
+  deletePost(req, res)
 );
 accessControlRoutes.put("/post/update", tokenVerification, (req, res) =>
-	updatePost(req, res),
+  updatePost(req, res)
 );
 
 // no auth needed routes
 accessControlRoutes.get("/user/:username", (req, res) =>
-	loadUserPosts(req, res),
+  loadUserPosts(req, res)
 );
 accessControlRoutes.get("/all", (req, res) => allPosts(req, res));
 
